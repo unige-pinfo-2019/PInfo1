c481176503b0d9cb244f4531d979b82341aaa8cb
sardinhapatrick <Patrick.Sardinha@etu.unige.ch>
2019-04-26 12:05:46 +0200

    add routing home & catalogue

--- web-ui/src/app/app.module.ts
+++ web-ui/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
+import { Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 import { NavBarComponent } from './pages/nav-bar/nav-bar.component';
-import { HighBodyComponent } from './pages/high-body/high-body.component';
-import { LowBodyComponent } from './pages/low-body/low-body.component';
-import { QuickSearchComponent } from './pages/quick-search/quick-search.component';
+import { HighBodyComponent } from './pages/home/high-body/high-body.component';
+import { LowBodyComponent } from './pages/home/low-body/low-body.component';
+import { QuickSearchComponent } from './pages/home/quick-search/quick-search.component';
 import { FooterComponent } from './pages/footer/footer.component';
-import { CarouselItemComponent } from './pages/carousel-item/carousel-item.component';
-import { AnnonceBottomComponent } from './pages/annonce-bottom/annonce-bottom.component';
+import { CarouselItemComponent } from './pages/home/carousel-item/carousel-item.component';
+import { AnnonceBottomComponent } from './pages/home/annonce-bottom/annonce-bottom.component';
+import { FiltersComponent } from './pages/catalogue/filters/filters.component';
+import { BodyHomeComponent } from './pages/home/body-home/body-home.component';
+
+const appRoutes: Routes = [
+    { path: '', component: BodyHomeComponent },
+    { path: 'home', component: BodyHomeComponent },
+    { path: 'catalogue', component: FiltersComponent }
+];
 
 @NgModule({
   declarations: [
@@ -23,11 +34,15 @@ import { AnnonceBottomComponent } from './pages/annonce-bottom/annonce-bottom.co
     QuickSearchComponent,
     FooterComponent,
     CarouselItemComponent,
-    AnnonceBottomComponent
+    AnnonceBottomComponent,
+    FiltersComponent,
+    BodyHomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
     NgbModule.forRoot()
   ],
   providers: [],
