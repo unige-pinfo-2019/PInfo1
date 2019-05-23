35e5adc83f1f4229bece6be1a2e617e641552c66
sardinhapatrick <Patrick.Sardinha@etu.unige.ch>
2019-05-23 13:39:33 +0200

    modification profil page & routing

---
+++ web-ui/src/app/pages/profil/vente-perso/vente-perso.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { CatalogueService } from '../../../services/catalogue.service'
+import { Observable, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-vente-perso',
+  templateUrl: './vente-perso.component.html',
+  styleUrls: ['./vente-perso.component.scss']
+})
+export class VentePersoComponent implements OnInit {
+
+  private list_items_user: any[];
+  //message: string = "?category=all"; --->
+  message: string = "1234";
+
+  constructor(private catalogueService: CatalogueService) { }
+
+  ngOnInit() {
+    this.catalogueService.currentMessage_usrid.subscribe((res) => {
+    this.message = res;
+    this.catalogueService.get_item_by_user(this.message).subscribe((res: any[]) => {
+      this.list_items_user = res;
+      console.log(res);
+  })})
+  }
+
+}
