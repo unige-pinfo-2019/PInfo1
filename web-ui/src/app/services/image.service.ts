72c1b8fe41ef8df47a79ca6702044b26c126a03c
AdrienCHABERT <adrien.chabert@etu.unige.ch>
2019-05-28 13:18:53 +0200

    envir

---
+++ web-ui/src/app/services/image.service.ts
@@ -0,0 +1,28 @@
+// import { Client } from '@rmp135/imgur'
+//
+//
+// @Injectable()
+// export class ImageService {
+// // The client can be initialised with the client_id.
+//
+// let client = new Client('43652b743b5a7a0')
+//
+// // Or with full authorization information.
+//
+// // client = new Client({
+// //   access_token: 'be3fb3e75b229254c8a44e41e376bfc01154ba42',
+// //   client_id: '43652b743b5a7a2',
+// //   client_secret: '2817603add0052fc9920bd3896abdd26992cf422',
+// //   refresh_token: '71f0489801d0d6ae906f51ea20ae562b2ef18d42'
+// // })
+// constructor(private httpClient: HttpClient) {}
+//
+//
+// get_image() {
+//
+// console.log("On essaie :).");
+// const images = await client.Gallery.subRedditGalleries('pics');
+//
+// console.log(images);
+// }
+// }
--- web-ui/src/environments/environment.ts
+++ web-ui/src/environments/environment.ts
@@ -11,15 +11,15 @@
 
 export const environment = {
     production: false,
-    // items_url: 'https://localhost/api/items',
-    // user_url: 'https://localhost/api/user',
-    // ad_url: 'https://localhost/api/ad',
-    // statistic_url: 'https://localhost/api/statistic',
+    items_url: 'https://localhost/api/items',
+    user_url: 'https://localhost/api/user',
+    ad_url: 'https://localhost/api/ad',
+    statistic_url: 'https://localhost/api/statistic',
 
-    user_url: 'http://localhost:12080',
-    items_url: 'http://localhost:10080',
-    ad_url: 'http://localhost:11080',
-    statistic_url: 'http://localhost:14080',
+    // user_url: 'http://localhost:12080',
+    // items_url: 'http://localhost:10080',
+    // ad_url: 'http://localhost:11080',
+    // statistic_url: 'http://localhost:14080',
 
 
 
