1f29daa6fb04b048eb3d6bf084f3c8eaab375bd0
AdrienCHABERT <adrien.chabert@etu.unige.ch>
2019-06-03 22:45:43 +0200

    image

--- web-ui/src/app/pages/load-image/load-image.component.ts
+++ web-ui/src/app/pages/load-image/load-image.component.ts
@@ -3,26 +3,44 @@ import { Router } from '@angular/router'
 import { HttpHeaders, HttpClient ,HttpParams } from '@angular/common/http';
 
 
-
 @Component({
   selector: 'app-load-image',
   templateUrl: './load-image.component.html',
   styleUrls: ['./load-image.component.scss']
 })
+
+
+
 export class LoadImageComponent implements OnInit {
   selectedFile = null;
 
+  httpOptions = {
+    headers: new HttpHeaders({
+        'Authorization': 'Client-ID 4fb3f8deeec4486',}),
+  };
+
+
+
   onFIleSelected(event){
-    this.selectedFile = event.target.files[0]
-    console.log(this.selectedFile);
+
+      this.selectedFile = event.target.files[0]
+      console.log(this.selectedFile);
+
   }
 
   onUpload(){
-
+    const fd = new FormData();
+    fd.append('image', this.selectedFile, this.selectedFile.name)
+    this.httpClient.post('https://api.imgur.com/3/image',fd, this.httpOptions).subscribe((res)=>{
+      console.log('Saved ! ');
+      console.log(res);
+      },(error) => {console.log('Erreur  ! : '+ error);}
+    );
   }
 
-  constructor(private http: HttpClient, private router: Router) { }
 
+
+  constructor(private router: Router, private httpClient: HttpClient) {  }
 
   ngOnInit() {
   }
