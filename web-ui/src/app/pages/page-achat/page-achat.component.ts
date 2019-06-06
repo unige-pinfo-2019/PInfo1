070379e3ffce50be0582a9ad3037888b047a1a00
AdrienCHABERT <adrien.chabert@etu.unige.ch>
2019-06-06 17:07:23 +0200

    modif

--- web-ui/src/app/pages/page-achat/page-achat.component.ts
+++ web-ui/src/app/pages/page-achat/page-achat.component.ts
@@ -50,7 +50,8 @@ export class PageAchatComponent implements OnInit {
   message(){
     console.log("Je veux envoyer un message au vendeur");
       //this.putService.modifPost(this.item);
-    this.router.navigate(['messagerie/discussion/'+this.items.usrId]);
+    console.log('discussion/'+this.items.usrId);
+    this.router.navigate(['../../../discussion/'+this.items.usrId]);
 
   }
 
