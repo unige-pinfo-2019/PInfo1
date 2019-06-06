c0d869a44a6187014d6679b00d072c62ac2042f1
TommasoPeletta <tommaso.peletta@etu.unige.ch>
2019-06-06 16:34:14 +0200

    add report to user and image

--- user-service/src/main/java/api/UserRestService.java
+++ user-service/src/main/java/api/UserRestService.java
@@ -2,21 +2,18 @@ package api;
 
 import java.net.URI;
 import java.util.List;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import api.msg.UserProducer;
 import domain.model.Users;
 import domain.service.UserService;
 
@@ -28,8 +25,6 @@ public class UserRestService {
 	@Inject
 	private UserService userservice;
 	
-	@Inject
-	private UserProducer userproducer;
 	
 	public void setUserservice(UserService us) {
 		userservice = us;
@@ -43,28 +38,25 @@ public class UserRestService {
 	public List<Users> getAll() {
 		return userservice.getAll();
 	}
-	
-	@POST
+
+	@PUT
 	@Consumes("application/json")
-	public Response create(Users us) {
-		Users usr = new Users(us.getName(), us.getSurname(), us.getUsername(),us.getEmail(),us.getReport(),us.getGrade());
+	public Response incremetReport(String id,String idReport) {
 		try {
-			userservice.create(usr);
+			userservice.incrementReport(id,idReport);
 		} catch(IllegalArgumentException i) {
 			return Response.status(Status.BAD_REQUEST).build();
 		} catch(Exception e) {
 			return Response.status(Status.BAD_GATEWAY).build();
 		}
-		userproducer.sendUser(usr, "adduser");
 		return Response.status(Status.CREATED).location(URI.create("/home")).build();
 	}
 	
 	@PUT
 	@Consumes("application/json")
-	public Response updateUser(Users us) {
-		Users usr = new Users(us.getId(), us.getName(), us.getSurname(), us.getUsername(),us.getEmail(),us.getReport(),us.getGrade());
+	public Response updateImage(String id,String image) {
 		try {
-			userservice.updateUser(usr);
+			userservice.updateImage(id,image);
 		} catch(IllegalArgumentException i) {
 			return Response.status(Status.BAD_REQUEST).build();
 		} catch(Exception e) {
@@ -72,58 +64,7 @@ public class UserRestService {
 		}
 		return Response.status(Status.CREATED).location(URI.create("/home")).build();
 	}
-	
-	@GET
-	@Path("/adduser")
-	@Produces("application/json")
-	public List<Users> addUsers() {
-		userservice.addUsers();
-		return userservice.getAll();
-	}
-	
-	
-	@GET
-	@Path("/getuser")
-	@Produces("text/plain")
-	public String selectUser(@QueryParam("name")String name,
-			@QueryParam("surname")String surname) {
-				return userservice.getByNames(name,surname).toString();
 
-	}
-	
-	@GET
-	@Path("/getuserid")
-	@Produces("application/json")
-	public List<Users> selectUserID(@QueryParam("id")String strid) {
-		return userservice.getById2(strid);
-
-	}
-	
-	
-	@GET
-	@Path("/removeuser")
-	@Produces("text/plain")
-	public String deleteUser(@QueryParam("id") String strid ) {
-		userproducer.sendUserbyid(strid,"removeuser");
-		return userservice.removeUser(strid);
-	}
-	
-	
-	@GET
-	@Path("/updateuser")
-	@Produces("text/plain")
-	public String update(@QueryParam("id") String strid, @QueryParam("name")String name,
-			@QueryParam("surname")String surname,@QueryParam("username")String username,
-			@DefaultValue("") @QueryParam("email")String email,
-			@DefaultValue("0") @QueryParam("report")int report
-			) {
-		Boolean tmp = userservice.modifyUser(strid,name,surname,username,email,report);
-		if (tmp) {
-			return "L'utilisateur a été modifié \n";
-		} 
-		return "L'utilisateur est introuvable";
-		
-	}
 	
 
 }
